e6eaa8d4667b92a86bb17e7b2ff19a609fee7781
coolgourav147 <coolgourav147@gmail.com>
2022-06-21 11:20:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ function sleep(ms) {
 
 
 async function init() {
-  await sleep(60000);
+  // await sleep(60000);
   app.listen(port, () =>{
     console.log(`Hey Gaurav! Your Application start listening on port ${port}`)
 })
